a9240293d9b16075e5a59f8f37b9011723459400
AlekseyAleksandrov <axcerz@gmail.com>
2025-06-10 11:12:15 +0300

    change: database.js

--- server/db/database.js
+++ server/db/database.js
@@ -11,8 +11,5 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false },
-    },
   },
 };
