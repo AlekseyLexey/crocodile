f669a65bc0ba42c235f4b6629b7db7cc0dcab525
AlekseyAleksandrov <axcerz@gmail.com>
2025-06-10 11:03:32 +0300

    add: sequelize log

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const serverConfig = require("./config/serverConfig");
 const indexRoutes = require("./routes/indexRoutes");
 const errorMiddleware = require("./middlewares/errorMiddleware");
+const { sequelize } = require("../db/models");
+
+sequelize.authenticate(console.log("BD connection"));
 
 const app = express();
 
