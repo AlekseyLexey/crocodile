c73c8d7334e05fe1a8382fb37f4d3ef4d081b120
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-06-12 21:41:38 +0300

    create: modal for burger. Change: button logout

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -4,15 +4,21 @@ import styles from "./Header.module.scss";
 import { useAppSelector } from "@/shared/hooks/useReduxHooks";
 import { useAppDispatch } from "@/shared/hooks/useReduxHooks";
 import { logoutThunk } from "@/entities/user";
+import { useState } from "react";
 
 export const Header = () => {
   const user = useAppSelector((state) => state.user.user);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = async () => {
     await dispatch(logoutThunk());
     navigate(CLIENT_ROUTES.SIGN_IN);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const isActive = ({ isActive }: { isActive: boolean }) =>
@@ -34,13 +40,60 @@ export const Header = () => {
 
       {user && (
         <>
-          <h3>Hello, {user.username}</h3>
+          <div className={styles.menuContainer}>
+            
+            <button
+              className={styles.burgerButton}
+              onClick={toggleMenu}
+              aria-label="Toggle-menu"
+            >
+              ☰
+            </button>
+
+            {isMenuOpen && (
+              <div className={styles.modalMenu}>
+                <Link
+                  to={CLIENT_ROUTES.SHOP}
+                  className={styles.menuItem}
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Магазин
+                </Link>
+                <Link 
+                  to={CLIENT_ROUTES.GAME}
+                  className={styles.menuItem}
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Игры
+                </Link>
+                <Link
+                to={CLIENT_ROUTES.PROFILE}
+                className={styles.menuItem}
+                onClick={() => setIsMenuOpen(false)}
+                >
+                  Профиль
+                </Link>
+              </div>
+            )}
+          </div>
+
+          <div className={styles.logoutContainer}>
+              <button 
+              onClick={handleLogout}
+              className={styles.logoutButton}
+              >
+                Выйти
+              </button>
+          </div>
+            
+
+          {/* <h3>Hello, {user.username}</h3>
           <Link to={CLIENT_ROUTES.MAIN} onClick={handleLogout}>
             Sign Out
           </Link>
           <Link to={CLIENT_ROUTES.GAME}>
             GamePage
-          </Link>
+          </Link> */}
         </>
       )}
     </nav>
