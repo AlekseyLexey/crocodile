7da35c10ec6664640b60a249d5bbdf09fe8486f6
AlekseyAleksandrov <axcerz@gmail.com>
2025-06-13 23:25:05 +0300

    fix: ws path

--- client/src/app/store/SocketProvider.tsx
+++ client/src/app/store/SocketProvider.tsx
@@ -4,7 +4,12 @@ import { io, Socket } from "socket.io-client";
 import SocketContext from "./SocketContex";
 
 export function SocketProvider({ children }: { children: React.ReactNode }) {
-  const [socket, setSocket] = useState<Socket>(io("ws://crocdraw.ru/ws"));
+  const [socket, setSocket] = useState<Socket>(
+    io("ws://crocdraw.ru", {
+      transports: ["websocket"],
+      path: "/ws",
+    })
+  );
 
   return (
     <SocketContext.Provider value={{ socket, setSocket }}>
