cafa9d987212fb0b02ae8c9529a15ebf6740c417
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-06-16 23:31:14 +0300

    fix ProfilePage

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -3,44 +3,42 @@ import styles from "./ProfilePage.module.scss";
 
 export const ProfilePage = () => {
   const userData = {
-    username: "GamoTron",
+    username: "NoHomo",
     avatar: "https://example.com/avatar.jpg",
     lastGames: [
-      { id: 1, date: "2023-05-15", score: 10},
-      { id: 2, date: "2023-05-10", score: 8},
-      { id: 3, date: "2023-05-05", score: 12},
+      { id: 1, score: 10},
+      { id: 2, score: 8},
+      { id: 3, score: 12},
     ],
   };
 
   return (
     <div className={styles.profile}>
       {/* Форма аватара имени и паролей */}
-      <div className={styles.profileHeader}>
-        <div className={styles.avatarContainer}>
-          <img src={userData.avatar} alt="Аватар"/>
-        </div>
-        <h1 className={styles.username}>{userData.username}</h1>
-
+      <div className={styles.section1}>
+        <img src={userData.avatar} alt="Аватар" className={styles.avatarContainer}/>
         {/* Пароли */}
-        <div className={styles.section}>
-          <h2 className={styles.sectionTitle}>Смена пароля</h2>
-          <form>
-            <Input type="text" placeholder="Имя" className={styles.input} />
-            <Input type="password" placeholder="Старый пароль" className={styles.input} />
-            <Input type="password" placeholder="Новый пароль" className={styles.input} />
+        <div className={styles.password}>
+          {/* <h2 className={styles.sectionTitle}>Смена пароля</h2> */}
+            
+            
+            <Input type="text" placeholder="Имя" className={styles.input} labelText="Имя" />
+            <Input type="email" placeholder="Email" className={styles.input} labelText="Email" />
+            <Input type="password" placeholder="Старый пароль" className={styles.input} labelText="Старый пароль" />
+            <Input type="password" placeholder="Новый пароль" className={styles.input} labelText="Новый пароль"/>
             <Button type="submit" buttonText="Сохранить" className={styles.submitButton} />
-          </form>
+          
         </div>
       </div>
 
       {/* Форма последних игр */}
-      <div className={styles.section}>
+      <div className={styles.section2}>
         <h2 className={styles.sectionTitle}>Результаты прошлых игр</h2>
         <div className={styles.gamesHistory}>
+            
           {userData.lastGames.map((game) => (
             <div key={game.id}>
-              <div>{game.date}</div>
-              <div>Очки: {game.score}</div>
+              <div className={styles.results}>Очки: {game.score}</div>
             </div>
           ))}
         </div>
