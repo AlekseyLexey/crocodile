fd3c6292de80de3a49c031dd3f12fca64901c075
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-06-17 22:31:31 +0300

    add: adaptive version

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,44 +1,67 @@
 import { Button, Input } from "@/shared";
 import styles from "./ProfilePage.module.scss";
+import { useEffect, useState } from "react";
 
 export const ProfilePage = () => {
+  const [showSection2, setShowSection2] = useState(false);
   const userData = {
     username: "NoHomo",
-    avatar: "https://example.com/avatar.jpg",
+    avatar: " ",
     lastGames: [
       { id: 1, score: 10},
       { id: 2, score: 8},
       { id: 3, score: 12},
+      { id: 3, score: 12},
+      { id: 3, score: 12},
+  { id: 3, score: 12},
+  { id: 3, score: 12},
+  { id: 3, score: 12},
     ],
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth <= 768) {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition > 50) {
+          setShowSection2(true);
+        } else {
+          setShowSection2(false);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
 
   return (
     <div className={styles.profile}>
       {/* Форма аватара имени и паролей */}
-      <div className={styles.section1}>
-        <img src={userData.avatar} alt="Аватар" className={styles.avatarContainer}/>
+      <div className={`${styles.section1} ${showSection2 ? styles.hideSection1 : ''}`}>
+        <img src={userData.avatar}  className={styles.avatarContainer}/>
         {/* Пароли */}
         <div className={styles.password}>
-          {/* <h2 className={styles.sectionTitle}>Смена пароля</h2> */}
+                  
             
-            
-            <Input type="text" placeholder="Имя" className={styles.input} labelText="Имя" />
-            <Input type="email" placeholder="Email" className={styles.input} labelText="Email" />
-            <Input type="password" placeholder="Старый пароль" className={styles.input} labelText="Старый пароль" />
-            <Input type="password" placeholder="Новый пароль" className={styles.input} labelText="Новый пароль"/>
+            <Input type="text"  className={styles.input} labelText="Имя" />
+            <Input type="email"  className={styles.input} labelText="Email" />
+            <Input type="password"  className={styles.input} labelText="Старый пароль" />
+            <Input type="password"  className={styles.input} labelText="Новый пароль"/>
             <Button type="submit" buttonText="Сохранить" className={styles.submitButton} />
           
         </div>
       </div>
 
       {/* Форма последних игр */}
-      <div className={styles.section2}>
+      <div className={`${styles.section2} ${showSection2 ? styles.showSection2 : ''}`}>
         <h2 className={styles.sectionTitle}>Результаты прошлых игр</h2>
-        <div className={styles.gamesHistory}>
+        <div className={styles.scrollContainer}>
             
           {userData.lastGames.map((game) => (
-            <div key={game.id}>
-              <div className={styles.results}>Очки: {game.score}</div>
+            <div className={styles.gamesHistory} key={game.id}>
+              <div className={styles.results}> Очки: {game.score}</div>
             </div>
           ))}
         </div>
