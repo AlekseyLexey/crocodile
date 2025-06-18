5b07ecf1659c3a3fdb000bc062201251c4d795a1
Пелагия Железняк <pelk@ya.ru>
2025-06-18 23:59:46 +0300

    change: lobby,modal,gamePage,tools

--- client/src/widgets/LobbyList/LobbyList.tsx
+++ client/src/widgets/LobbyList/LobbyList.tsx
@@ -11,6 +11,9 @@ import styles from './LobbyList.module.scss';
 import { getAllRoomThunk } from '@/entities/room/api/RoomApi';
 import type { IRoom } from '@/entities/room';
 import { useNavigate } from 'react-router-dom';
+import lionSvg from '@/assets/svg/animals/лев.svg';
+import crabSvg from '@/assets/svg/animals/краб.svg';
+import whaleSvg from '@/assets/svg/animals/кит.svg';
 
 export const LobbyList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,10 +30,6 @@ export const LobbyList = () => {
     }
   }, [dispatch]);
 
-  if (!rooms.length) {
-    return <h2>Лобби пока что нет...</h2>;
-  }
-
   const handleJoinGame = (id: number) => {
     navigate(`${CLIENT_ROUTES.GAME}/${id}`);
   };
@@ -38,6 +37,15 @@ export const LobbyList = () => {
   return (
     <>
       <div className={styles.pageContainer}>
+
+        {/* Десктопные животные */}
+        <img src={lionSvg} alt="Лев" className={styles.lionDesktop} />
+        <img src={crabSvg} alt="Краб" className={styles.crabDesktop} />
+        <img src={whaleSvg} alt="Кит" className={styles.whaleDesktop} />
+        
+        {/* Мобильное животное */}
+        <img src={lionSvg} alt="Лев" className={styles.lionMobile} />
+
         <div className={styles.contentWrapper}>
           <h1 className={styles.title}>Доступные игры</h1>
 
