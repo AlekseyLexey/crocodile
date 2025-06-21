934cd744481907b3d47e5a10c531d8d1aa92eb15
AlekseyAleksandrov <axcerz@gmail.com>
2025-06-21 09:00:56 +0300

    add: clear server stores (words, timer, leaveTimer) after end of game

--- server/src/sockets/helpers/gameEndAction.js
+++ server/src/sockets/helpers/gameEndAction.js
@@ -1,10 +1,14 @@
+const { leaveTimerStore } = require("./handleLeaveRoom");
 const { sendRoom } = require("./sendRoom");
 const { clearTimer } = require("./timerStore");
 const updateRoomsWithUserProfilePoints = require("./updateRoomsWithUserProfilePoints");
+const { clearRoomWords, roomWords } = require("./wordStore");
 
 module.exports.gameEndAction = async (io, socket, roomId) => {
   const room = await updateRoomsWithUserProfilePoints(roomId);
   clearTimer(roomId);
+  clearRoomWords(roomId);
+  leaveTimerStore?.delete(roomId);
 
   await sendRoom(io, roomId, room);
   socket.leave(roomId);
