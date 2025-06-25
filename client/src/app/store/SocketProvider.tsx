19afb4f5142f1a96dec92b44c155510d20c81692
AlekseyAleksandrov <axcerz@gmail.com>
2025-06-25 04:13:01 +0300

    Release v2.0.0

--- client/src/app/store/SocketProvider.tsx
+++ client/src/app/store/SocketProvider.tsx
@@ -4,15 +4,15 @@ import { io, Socket } from "socket.io-client";
 import SocketContext from "./SocketContex";
 
 export function SocketProvider({ children }: { children: React.ReactNode }) {
-  // const [socket, setSocket] = useState<Socket>(
-  //   io("wss://crocdraw.ru", {
-  //     transports: ["websocket"],
-  //     path: "/socket.io",
-  //     secure: true,
-  //   })
-  // );
+  const [socket, setSocket] = useState<Socket>(
+    io("wss://crocdraw.ru", {
+      transports: ["websocket"],
+      path: "/socket.io",
+      secure: true,
+    })
+  );
 
-  const [socket, setSocket] = useState<Socket>(io("ws://localhost:3000"));
+  // const [socket, setSocket] = useState<Socket>(io("ws://localhost:3000"));
 
   return (
     <SocketContext.Provider value={{ socket, setSocket }}>
